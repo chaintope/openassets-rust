c2b413788eea9a85fb031fd70fe3a17ecfb7cdce
azuchi <azuchi@chaintope.com>
2019-05-07 10:54:15 +0900

    Add AssetId#new

--- src/openassets/asset_id.rs
+++ src/openassets/asset_id.rs
@@ -3,12 +3,21 @@ use std::fmt::{self, Display, Formatter};
 use std::str::FromStr;
 use bitcoin::consensus::encode;
 use bitcoin::util::base58;
+use bitcoin::Script;
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct AssetId {
 
     pub hash: bitcoin_hashes::hash160::Hash,
-    pub network: bitcoin::network::constants:: Network
+    pub network: bitcoin::network::constants::Network
+
+}
+
+impl AssetId {
+
+    pub fn new(script: &Script, network: bitcoin::network::constants::Network) -> AssetId{
+        AssetId { hash: hash160::Hash::hash(&script.to_bytes()), network}
+    }
 
 }
 
@@ -51,17 +60,16 @@ mod tests {
     use bitcoin::blockdata::script::Builder;
     use openassets::asset_id::AssetId;
     use hex::decode as hex_decode;
-    use bitcoin_hashes::{hash160, Hash};
 
     #[test]
     fn test_calculate_asset_id() {
         let p2pkh = Builder::from(hex_decode("76a914010966776006953d5567439e5e39f86a0d273bee88ac").unwrap()).into_script();
-        let p2pkh_asset = AssetId { hash: hash160::Hash::hash(&p2pkh.to_bytes()), network: bitcoin::network::constants::Network::Bitcoin };
+        let p2pkh_asset = AssetId::new(&p2pkh, bitcoin::network::constants::Network::Bitcoin);
         assert_eq!("ALn3aK1fSuG27N96UGYB1kUYUpGKRhBuBC", p2pkh_asset.to_string());
         assert_eq!(AssetId::from_str("ALn3aK1fSuG27N96UGYB1kUYUpGKRhBuBC").unwrap(), p2pkh_asset);
 
         let p2sh  = Builder::from(hex_decode("a914f9d499817e88ef7b10a88673296c6d6df2f4292d87").unwrap()).into_script();
-        let testnet_asset = AssetId { hash: hash160::Hash::hash(&p2sh.to_bytes()), network: bitcoin::network::constants::Network::Testnet };
+        let testnet_asset = AssetId::new(&p2sh, bitcoin::network::constants::Network::Testnet);
         assert_eq!("oMb2yzA542yQgwn8XtmGefTzBv5NJ2nDjh", testnet_asset.to_string());
         assert_eq!(AssetId::from_str("oMb2yzA542yQgwn8XtmGefTzBv5NJ2nDjh").unwrap(), testnet_asset);
     }
